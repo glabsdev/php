7009b9bb988ad68a84aee2a5b81bda01b6b980c6
glabsdev <53467246+glabsdev@users.noreply.github.com>
2019-08-08 15:54:01 +0800

    Add files via upload

--- load_api.php
+++ load_api.php
@@ -1,4 +1,11 @@
 <?php
+
+$app_id = 'B54z9Ug5512HHrTGRT5g6hq64pGUq6ap';
+$app_secret = 'f655413712345607a696cd40741993758c411af3bb5f6c230270ec26e8d54126';
+$rewards_token = 'AbCkxKYid_F_p-JSgTejow';
+$address = '9001234567';
+$promo = 'LOAD 1';
+
 $curl = curl_init();
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://devapi.globelabs.com.ph/rewards/v1/transactions/send",
@@ -8,7 +15,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{\n \"outboundRewardRequest\" : {\n \"app_id\" : \"<app_id>\",\n \"app_secret\" : \"<app_secret> \",\n \"rewards_token\" : \"<rewards_token> \",\n \"address\" : \"<address>\",\n \"promo\" : \"<promo>\"\n    }\n}",
+  CURLOPT_POSTFIELDS => "{ \"outboundRewardRequest\" : { \"app_id\" : \"".$app_id."\", \"app_secret\" : \"".$app_secret."\", \"rewards_token\" : \"".$rewards_token."\", \"address\" : \"".$address."\", \"promo\" : \"".$promo."\" } }",
   CURLOPT_HTTPHEADER => array(
     "Content-Type: application/json",
     "Host: devapi.globelabs.com.ph"
--- load_api_status.php
+++ load_api_status.php
@@ -1,4 +1,10 @@
 <?php
+
+$app_id = 'B54z9Ug5512HHrTGRT5g6hq64pGUq6ap';
+$app_secret = 'f655413712345607a696cd40741993758c411af3bb5f6c230270ec26e8d54126';
+$rewards_token = 'AbCkxKYid_F_p-JSgTejow';
+$transaction_id = '1234567';
+
 $curl = curl_init();
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://devapi.globelabs.com.ph/rewards/v1/transactions/status",
@@ -8,7 +14,7 @@ curl_setopt_array($curl, array(
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => " {\"rewardStatusRequest\" : {\"app_id\" : \"<app_id>\", \"app_secret\" : \"<app_secret> \", \"rewards_token\" : \"<rewards_token> \", \"transaction_id\" : \"<transaction_id>\"} }",
+  CURLOPT_POSTFIELDS => "{\"rewardStatusRequest\" : {\"app_id\" : \"".$app_id."\", \"app_secret\" : \"".$app_secret."\", \"rewards_token\" : \"".$rewards_token."\", \"transaction_id\" : \"".$transaction_id."\" } }",
   CURLOPT_HTTPHEADER => array(
     "Content-Type: application/json"
   ),
